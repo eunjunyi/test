a221b58c4bed98e8eb6cce2fa83b6560ca8cda5c
eunjunyi <69071022+eunjunyi@users.noreply.github.com>
2021-08-07 23:09:34 +0900

    Delete basicboot directory

--- basicboot/src/main/java/com/mor/test/domain/base/TestTableRepository.java
+++
@@ -1,6 +0,0 @@
-package com.mor.test.domain.base;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestTableRepository extends JpaRepository<TestTable, Integer> {
-}
